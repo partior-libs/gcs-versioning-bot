b1752b94cd7974b11eca857fc3229f8a6437a8ae
Anthony Yeong <anthony.yeong@partior.com>
2022-03-04 00:14:48 +0800

    DSO-340 Start inject vars

---
+++ scripts/inject_config_vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash +e
+
+importerFile=$1
+
+if [[ -f "$importerFile" ]]; then
+    echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to locate importer file: [$importerFile]"
+    exit 1
+fi
+
+cat $importerFile | sed -r "s/echo ::set-output name=//g" | sed -r "s/::/=\"/g" | sed -r "s/$/\"/g" | grep "=\"" >> $GITHUB_ENV
