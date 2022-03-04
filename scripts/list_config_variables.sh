3460902ea7464645e20fc748f2949df8a740d43d
Anthony Yeong <anthony.yeong@partior.com>
2022-03-04 09:48:00 +0800

    DSO-340 debug vars parsing 7

--- scripts/list_config_variables.sh
+++ scripts/list_config_variables.sh
@@ -1,28 +1,30 @@
 #!/bin/bash +e
 
 ## Reading action's global setting
-if [[ ! -z $BASH_SOURCE ]]; then
-    ACTION_BASE_DIR=$(dirname $BASH_SOURCE)
-    source $(find $ACTION_BASE_DIR/.. -type f | grep general.ini)
-elif [[ $(find . -type f -name general.ini | wc -l) > 0 ]]; then
-    source $(find . -type f | grep general.ini)
-elif [[ $(find .. -type f -name general.ini | wc -l) > 0 ]]; then
-    source $(find .. -type f | grep general.ini)
-else
-    echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to find and source general.ini"
-    exit 1
-fi
+# if [[ ! -z $BASH_SOURCE ]]; then
+#     ACTION_BASE_DIR=$(dirname $BASH_SOURCE)
+#     echo find $ACTION_BASE_DIR/.. -type f 
+#     source $(find $ACTION_BASE_DIR/.. -type f | grep general.ini)
+# elif [[ $(find . -type f -name general.ini | wc -l) > 0 ]]; then
+#     source $(find . -type f | grep general.ini)
+# elif [[ $(find .. -type f -name general.ini | wc -l) > 0 ]]; then
+#     source $(find .. -type f | grep general.ini)
+# else
+#     echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to find and source general.ini"
+#     exit 1
+# fi
 
 
 iniFile=$1
+
 tempRunFile=run-$(date +%s).sh
 
 if [[ ! -f "$iniFile" ]]; then
     echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to locate INI file: [$iniFile]"
     exit 1
 fi
-
-head -15 $BASH_SOURCE > $tempRunFile
+source $iniFile
+echo "source $iniFile" > $tempRunFile
 # cat $iniFile | grep -v "^#" | grep "=" | awk -F'=' '{print "export "$1"="$2 }' > $tempRunFile
 # source ./$tempRunFile
 
@@ -31,4 +33,4 @@ chmod 755 $tempRunFile
 
 echo "[INFO] Listing variables..."
  ./$tempRunFile
-rm -f $tempRunFile
+# rm -f $tempRunFile
