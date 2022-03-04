bc315a04a85c085beeaac7f8f20b87f04a615afa
Anthony Yeong <anthony.yeong@partior.com>
2022-03-04 09:59:34 +0800

    DSO-340 try versioning

--- scripts/list_config_variables.sh
+++ scripts/list_config_variables.sh
@@ -1,20 +1,5 @@
 #!/bin/bash +e
 
-## Reading action's global setting
-# if [[ ! -z $BASH_SOURCE ]]; then
-#     ACTION_BASE_DIR=$(dirname $BASH_SOURCE)
-#     echo find $ACTION_BASE_DIR/.. -type f 
-#     source $(find $ACTION_BASE_DIR/.. -type f | grep general.ini)
-# elif [[ $(find . -type f -name general.ini | wc -l) > 0 ]]; then
-#     source $(find . -type f | grep general.ini)
-# elif [[ $(find .. -type f -name general.ini | wc -l) > 0 ]]; then
-#     source $(find .. -type f | grep general.ini)
-# else
-#     echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to find and source general.ini"
-#     exit 1
-# fi
-
-
 iniFile=$1
 
 tempRunFile=run-$(date +%s).sh
@@ -25,12 +10,10 @@ if [[ ! -f "$iniFile" ]]; then
 fi
 source $iniFile
 echo "source $iniFile" > $tempRunFile
-# cat $iniFile | grep -v "^#" | grep "=" | awk -F'=' '{print "export "$1"="$2 }' > $tempRunFile
-# source ./$tempRunFile
 
 cat $iniFile | grep -v "^#" | grep "=" | awk -F'=' '{print "echo " $1"=${"$1"}" }' >> $tempRunFile
 chmod 755 $tempRunFile
 
 echo "[INFO] Listing variables..."
  ./$tempRunFile
-# rm -f $tempRunFile
+rm -f $tempRunFile
