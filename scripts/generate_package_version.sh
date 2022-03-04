7ee6bdd7b7d44c20b0d940de3c4e5aea77b8a6d5
Anthony Yeong <anthony.yeong@partior.com>
2022-03-04 17:33:07 +0800

    DSO-340 Fix pre-release increment

--- scripts/generate_package_version.sh
+++ scripts/generate_package_version.sh
@@ -52,9 +52,12 @@ function incrementPreReleaseVersion() {
     local inputVersion=$1
     local preIdentifider=$2
 
-    currentSemanticVersion=$(echo $inputVersion | awk -F"-$preIdentifider." '{print $1}')
-    nextPreReleaseNumber=$(( $(echo $inputVersion | awk -F"-$preIdentifider." '{print $2}') + 1 ))
-
+    local currentSemanticVersion=$(echo $inputVersion | awk -F"-$preIdentifider." '{print $1}')
+    local nextPreReleaseNumber=$(( $(echo $inputVersion | awk -F"-$preIdentifider." '{print $2}') + 1 ))
+    ## If not pre-release, then increment the core version too
+    if [[ "$inputVersion" == *"-"* ]]; then
+        currentSemanticVersion=$(incrementCoreVersion $currentSemanticVersion ${PATCH_POSITION})
+    fi
     echo $currentSemanticVersion-$preIdentifider.$nextPreReleaseNumber
 }
 
