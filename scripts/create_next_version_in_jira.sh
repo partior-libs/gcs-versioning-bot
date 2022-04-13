28a0469960c3bb5f02a38e1cf8627fd451ee892e
ArshPartior <101242136+ArshPartior@users.noreply.github.com>
2022-04-13 22:14:23 +0530

    DSO-522-Create the next version in jira

---
+++ scripts/create_next_version_in_jira.sh
@@ -0,0 +1,47 @@
+#!/bin/bash +e
+
+## Reading action's global setting
+if [[ ! -z $BASH_SOURCE ]]; then
+    ACTION_BASE_DIR=$(dirname $BASH_SOURCE)
+    source $(find $ACTION_BASE_DIR/.. -type f | grep general.ini)
+elif [[ $(find . -type f -name general.ini | wc -l) > 0 ]]; then
+    source $(find . -type f | grep general.ini)
+elif [[ $(find .. -type f -name general.ini | wc -l) > 0 ]]; then
+    source $(find .. -type f | grep general.ini)
+else
+    echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to find and source general.ini"
+    exit 1
+fi
+
+jiraProjectId=${15}
+
+cat $ARTIFACT_NEXT_VERSION_FILE
+echo $ARTIFACT_NEXT_VERSION_FILE > newversions.tmp
+createArtifactNextVersionInJira "newversions.tmp" "$DEV_V_IDENTIFIER"
+createArtifactNextVersionInJira "newversions.tmp" "$RC_V_IDENTIFIER"
+createArtifactNextVersionInJira "newversions.tmp" "$$REL_SCOPE"
+
+
+function createArtifactNextVersionInJira() {
+local newVersionsFile=$1
+local identifierType=$2
+local versionName=$(cat $newVersionsFile | grep $identifierType)
+if [[ ! -f "$newVersionsFile" ]]; then
+        echo "[ERROR] $BASH_SOURCE (line:$LINENO): Artifact list file not found: [$newVersionsFile]"
+
+        exit 1
+    fi
+	
+	response=$(curl -k -s -u $jiraUsername:$jiraPassword \
+				-w "status_code:[%{http_code}]" \
+				-X POST \
+				-H "Content-Type: application/json" \
+				--data '{"projectId" : $jiraProjectId,"name" : "$versionName","startDate" : null,"releaseDate" : null,"description" : ""}'
+				"$jiraBaseUrl/rest/api/2/version")
+				
+	if [[ $? -ne 0 ]]; then
+        echo "[ACTION_CURL_ERROR] $BASH_SOURCE (line:$LINENO): Error running curl to post the next version."
+        echo "[DEBUG] Curl: $jiraBaseUrl/rest/api/latest/project/$jiraProjectKey"
+        exit 1
+fi
+
