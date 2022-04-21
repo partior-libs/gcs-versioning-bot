68bb155af1b7388a3bfcd531278507651ecaf6df
ArshPartior <101242136+ArshPartior@users.noreply.github.com>
2022-04-21 22:08:17 +0530

    DSO-523-Jira issue integration with version

--- scripts/integrate_issue_with_version.sh
+++ scripts/integrate_issue_with_version.sh
@@ -25,16 +25,52 @@ echo "[INFO] Consolidated commit message: $deltaMessage"
 echo "[INFO] Jira project key: $projectKey"
 echo "[INFO] New version: $newVersion"
 
+# Extract all issue keys from consolidated commit message 
 function extractIssueKey() {
-    echo "[INFO] Inside the function"
     deltaMessage=$1
     projectKey=$2
-    local issueKeys=()
     for word in $deltaMessage; do
-        issueKeys+=("$(echo $word | grep -i "devpilot" | cut -d "-" --complement -f 3 | tr 'a-z' 'A-Z')") 
+        issueKeys+=("$(echo $word | grep -i "$projectKey" | cut -d "-" --complement -f 3 | tr 'a-z' 'A-Z')")  # Grep and Convert all keys to uppercase
     done
-    issueKeys=($(echo "${issueKeys[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+    issueKeys=($(echo "${issueKeys[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))  # Store only distinct keys 
     echo ${issueKeys[@]} 
 }
 
+function updateIssueWithVersion() {
+    local newVersion=$1
+    local jiraBaseUrl=$2
+    local response=""
+    for issueKey in ${issueKeys[@]}; do
+        response=$(curl -k -s -u $jiraUsername:$jiraPassword \
+                -w "status_code:[%{http_code}]" \
+                -X PUT \
+                -H "Content-Type: application/json" \
+                --data '{"update" : { "fixVersions":[{ "add": { "name": "'$newVersion'" }}]}}' \
+                "https://$jiraBaseUrl/rest/api/2/issue/$issueKey")
+                
+    if [[ $? -ne 0 ]]; then
+        echo "[ACTION_CURL_ERROR] $BASH_SOURCE (line:$LINENO): Error running curl to post the next version."
+        echo "[DEBUG] Curl: https://$jiraBaseUrl/rest/api/2/issue/$issueKey"
+        echo "$response"
+        exit 1
+    fi
+    
+    local responseStatus=$(echo $response | awk -F'status_code:' '{print $2}' | awk -F'[][]' '{print $2}')
+    #echo "[INFO] responseBody: $responseBody"
+    echo "[INFO] Query status code: $responseStatus"
+
+    if [[ $responseStatus -eq 204 ]]; then
+        echo "[INFO] Response status $responseStatus"
+        echo "[INFO] Issue update with fix version "
+
+    else
+        echo "[ACTION_RESPONSE_ERROR] $BASH_SOURCE (line:$LINENO): Return code not 204 when updating issue with new version in jira: [$responseStatus]" 
+        echo "[ERROR] $(echo $response) "
+		exit 1
+    fi
+}
+
+}
+local issueKeys=()
 extractIssueKey "$deltaMessage" "$projectKey"
+updateIssueWithVersion "$newVersion" "$jiraBaseUrl"
