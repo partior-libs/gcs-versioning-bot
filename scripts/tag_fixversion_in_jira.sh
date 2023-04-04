c7691d51f12d7ec653ea3ac1393e23a60ff24042
Anthony Yeong <anthony.yeong@partior.com>
2023-04-04 07:26:35 +0000

    DSO-1588 Make the list uniq

--- scripts/tag_fixversion_in_jira.sh
+++ scripts/tag_fixversion_in_jira.sh
@@ -120,7 +120,7 @@ function startTaggingFixVersion() {
     fi
 
     # Extract Jira ticket IDs using regex and write to file
-    cat "${messageFile}" | grep -oP '([A-Z]+-[0-9]+)+' | tr '\n' ',' | sed 's/,$//' > "${jiraListFile}"
+    cat "${messageFile}" | grep -oP '([A-Z]+-[0-9]+)+' | uniq | tr '\n' ',' | sed 's/,$//' > "${jiraListFile}"
     
     # Loop through ticket IDs and do something
     for eachJiraIssue in $(cat "${jiraListFile}" | tr ',' ' '); do
