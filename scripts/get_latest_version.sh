628e69ea4dc9a91487c3e691f43601453a2e28bd
Anthony Yeong <anthony.yeong@partior.com>
2023-05-18 19:40:45 +0000

    DSO-1942 Fix grep flag

--- scripts/get_latest_version.sh
+++ scripts/get_latest_version.sh
@@ -365,7 +365,7 @@ function filterVersionListWithPrependVersion() {
                 echo "$resetVersion" > $versionOutputFile
             fi
         else
-            cat $versionOutputFile | grep "\"$inputPrependVersionLabel-\<[0-9]+\.[0-9]+\.[0-9]+\>" > $versionOutputFile.tmp
+            cat $versionOutputFile | grep -E "\"$inputPrependVersionLabel-\<[0-9]+\.[0-9]+\.[0-9]+\>" > $versionOutputFile.tmp
             mv $versionOutputFile.tmp $versionOutputFile
             echo "[INFO] List of versions after filtered"
             cat $versionOutputFile
